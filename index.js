09d14250c08b72ad31600574678b6bb8c5d4329b
Jared Lev Carranza <jacarranza@up.edu.ph>
2025-02-26 13:46:54 +0800

    feat: added generateUniqueID and addAccount

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
+import validator from 'validator';
+import { appendFileSync } from 'node:fs';
 
 function generateUniqueID(fname, lname) {
     //convert the first letter of fname and lname to lower case
@@ -9,4 +11,34 @@ function generateUniqueID(fname, lname) {
     return uniqueid;
 }
 
-console.log(generateUniqueID("Alan", "Turing"));
+function addAccount(fname, lname, email, age) {
+    //check if all are not null or empty
+    if (fname && lname && email && age) {
+        //check if email is valid format
+        if (validator.isEmail(email)) {
+            //check if age is at least 18
+            if (age >= 18) {
+                let uniqueid = generateUniqueID(fname, lname);
+                try {
+                    appendFileSync('users.txt', `${fname},${lname},${email},${uniqueid}\n`);
+                    console.log("Account Added!")
+                    return true
+                  } catch (err) {
+                    console.log("Account creation failed")
+                    return false
+                  }
+            }
+            else {
+                return false
+            }
+        }
+        else {
+            return false
+        }
+    }
+    else {
+        return false
+    }
+}
+
+export default {generateUniqueID, addAccount}
---
+++ test.js
@@ -0,0 +1,3 @@
+import index from './index.js'
+
+index.addAccount("Alan", "Turing", "alan@gmail.com", 18);
